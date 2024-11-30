54f0f59b650fe631f16012200b5d08ad85de3eb4
Abdul <haqhaq94@gmail.com>
2024-11-30 16:57:30 +0500

    Update submissions.component.ts

--- src/app/submissions/submissions.component.ts
+++ src/app/submissions/submissions.component.ts
@@ -63,26 +63,26 @@ export class SubmissionsComponent implements AfterViewInit {
   viewMode = 'map'; 
   platform: any; 
   svg = `<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g filter="url(#filter0_d_2281_9719)">
-        <circle cx="24" cy="20" r="20" fill="#2188D9"/>
-        <circle cx="24" cy="20" r="19" stroke="white" stroke-width="2"/>
-        </g>
-        <path d="M16.9688 32H31.0312C32.1944 32 33.1406 31.0538 33.1406 29.8906V15.0312H28.2188C27.0556 15.0312 26.1094 14.085 26.1094 12.9219V8H16.9688C15.8056 8 14.8594 8.94622 14.8594 10.1094V29.8906C14.8594 31.0538 15.8056 32 16.9688 32ZM19.0781 19.2969C19.0781 18.9082 19.3926 18.5938 19.7812 18.5938H28.2188C28.6074 18.5938 28.9219 18.9082 28.9219 19.2969V26.3281C28.9219 26.7168 28.6074 27.0312 28.2188 27.0312H19.7812C19.3926 27.0312 19.0781 26.7168 19.0781 26.3281V19.2969Z" fill="white"/>
-        <path d="M20.4844 20H27.5156V25.625H20.4844V20Z" fill="white"/>
-        <path d="M28.2188 13.6249H32.7286L27.5156 8.41187V12.9218C27.5156 13.3097 27.8308 13.6249 28.2188 13.6249Z" fill="white"/>
-        <defs>
-        <filter id="filter0_d_2281_9719" x="0" y="0" width="48" height="48" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-        <feOffset dy="4"/>
-        <feGaussianBlur stdDeviation="2"/>
-        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/>
-        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2281_9719"/>
-        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2281_9719" result="shape"/>
-        </filter>
-        </defs>
-        </svg>
-        `;
+<g filter="url(#filter0_d_2281_9719)">
+<circle cx="24" cy="20" r="20" fill="#2188D9"/>
+<circle cx="24" cy="20" r="19" stroke="white" stroke-width="2"/>
+</g>
+<path d="M16.9688 32H31.0312C32.1944 32 33.1406 31.0538 33.1406 29.8906V15.0312H28.2188C27.0556 15.0312 26.1094 14.085 26.1094 12.9219V8H16.9688C15.8056 8 14.8594 8.94622 14.8594 10.1094V29.8906C14.8594 31.0538 15.8056 32 16.9688 32ZM19.0781 19.2969C19.0781 18.9082 19.3926 18.5938 19.7812 18.5938H28.2188C28.6074 18.5938 28.9219 18.9082 28.9219 19.2969V26.3281C28.9219 26.7168 28.6074 27.0312 28.2188 27.0312H19.7812C19.3926 27.0312 19.0781 26.7168 19.0781 26.3281V19.2969Z" fill="white"/>
+<path d="M20.4844 20H27.5156V25.625H20.4844V20Z" fill="white"/>
+<path d="M28.2188 13.6249H32.7286L27.5156 8.41187V12.9218C27.5156 13.3097 27.8308 13.6249 28.2188 13.6249Z" fill="white"/>
+<defs>
+<filter id="filter0_d_2281_9719" x="0" y="0" width="48" height="48" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2281_9719"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2281_9719" result="shape"/>
+</filter>
+</defs>
+</svg>
+`;
     icon = new H.map.Icon(this.svg, {
       anchor: { x: 24, y: 58 } // x: center horizontally, y: slightly below the bottom tip of the marker
   });
@@ -107,7 +107,7 @@ export class SubmissionsComponent implements AfterViewInit {
       from: 'denisgordiyenya@gmail.com',
       to: 'denisgordiyenya@gmail.com',
       dueDate:  '2024-12-09',
-      status: 'Uncomplete',
+      status: {key:'Uncomplete', value:'Uncomplete'},
       coordinates: {  lat: 43.6707974, lng: -79.347015 }
     },
     {
@@ -115,7 +115,7 @@ export class SubmissionsComponent implements AfterViewInit {
       from: 'john.doe@gmail.com',
       to: 'jane.doe@gmail.com',
       dueDate:  '2024-12-08',
-      status: 'Complete',
+      status: {key:'Complete', value:'Complete'},
       coordinates: { lat: 43.653908, lng: -79.384293}
     },
     {
@@ -123,7 +123,7 @@ export class SubmissionsComponent implements AfterViewInit {
       from: 'alice.smith@gmail.com',
       to: 'bob.smith@gmail.com',
       dueDate:  '2024-12-07',
-      status: 'Uncomplete',
+      status: {key:'Uncomplete', value:'Uncomplete'},
       coordinates: { lat:43.662301, lng: -79.329356 }
     },
     {
@@ -131,7 +131,7 @@ export class SubmissionsComponent implements AfterViewInit {
       from: 'alice.smith@gmail.com',
       to: 'bob.smith@gmail.com',
       dueDate: '2024-12-06',
-      status: 'Needs Review',
+      status: {key:'needreview', value:'Needs Review'},
       coordinates: {  lat: 43.662301, lng: -79.395293 }
     }
   ];
@@ -154,7 +154,7 @@ export class SubmissionsComponent implements AfterViewInit {
   get filteredSubmissions() {
     return this.submissions.filter((submission) => {
       const matchesForm = !this.selectedForm || submission.from === this.selectedForm;
-      const matchesStatus = !this.selectedStatus || submission.status === this.selectedStatus;
+      const matchesStatus = !this.selectedStatus || submission.status.value === this.selectedStatus;
       let matchesDate = true; 
       if (this.selectedDate) {
         const selectedDate = new Date(this.selectedDate).toDateString();
@@ -167,7 +167,7 @@ export class SubmissionsComponent implements AfterViewInit {
       submission.title.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
       submission.from.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
       submission.to.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-      submission.status.toLowerCase().includes(this.searchTerm.toLowerCase());
+      submission.status.value.toLowerCase().includes(this.searchTerm.toLowerCase());
 
       return matchesForm && matchesStatus && matchesDate && matchesSearchTerm;
       
