ced9e06bf95998f1799a5bed6d0abe77cd58dfbe
Abdul <haqhaq94@gmail.com>
2024-12-01 01:22:31 +0500

    Update submissions.component.ts

--- src/app/submissions/submissions.component.ts
+++ src/app/submissions/submissions.component.ts
@@ -1,16 +1,9 @@
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
-import { MatButtonToggleChange } from '@angular/material/button-toggle';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import H from '@here/maps-api-for-javascript';
 import onResize from 'simple-element-resize-detector';
-import * as _moment from 'moment';
-import {provideMomentDateAdapter} from '@angular/material-moment-adapter';
-import {default as _rollupMoment, Moment} from 'moment';
-import { FormControl} from '@angular/forms';
-import { MatDatepicker, MatDatepickerInputEvent} from '@angular/material/datepicker';
 import { MatIcon } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleGroup, MatButtonToggle } from '@angular/material/button-toggle';
 import {MatSelectModule} from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
@@ -22,45 +15,24 @@ import { MatInputModule } from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FlexLayoutModule } from 'ng-flex-layout';
-const moment = _rollupMoment || _moment;
-export const MY_FORMATS = {
-  parse: {
-    dateInput: 'MM/YYYY',
-  },
-  display: {
-    dateInput: 'DD/MM/YYYY',
-    monthYearLabel: 'MMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY',
-  },
-};
+
+
 @Component({
   selector: 'app-submissions',
   standalone: true,
-  imports:[MatIcon,MatListModule,MatSidenavModule,FlexLayoutModule,
-    MatFormFieldModule, MatButtonModule,FontAwesomeModule,MatNativeDateModule,MatInputModule ,MatDatepickerModule, CommonModule,MatSelectModule,MatDatepicker,MatButtonToggleGroup, MatButtonToggle,FormsModule,
-    ],
+  imports: [MatIcon, MatListModule, MatSidenavModule, FlexLayoutModule,
+    MatFormFieldModule, MatButtonModule, FontAwesomeModule, MatNativeDateModule, MatInputModule, MatDatepickerModule, CommonModule, MatSelectModule,  FormsModule],
   templateUrl: './submissions.component.html',
   styleUrl: './submissions.component.css',
-  providers: [
-    provideMomentDateAdapter(MY_FORMATS),
-  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SubmissionsComponent implements AfterViewInit {
-  readonly date = new FormControl(moment());
-
-  setMonthAndYear(normalizedMonthAndYear: Moment, datepicker: MatDatepicker<Moment>) {
-    const ctrlValue = this.date.value ?? moment();
-    ctrlValue.month(normalizedMonthAndYear.month());
-    ctrlValue.year(normalizedMonthAndYear.year());
-    this.date.setValue(ctrlValue);
-    datepicker.close();
-  }
+  @Input() submissions: any[];
+  @Input() filteredSubmissions: any[];
   private map?: H.Map;
   faicon = faCircle;
   @ViewChild('mapContainer', { static: false }) mapContainer !: ElementRef; 
-  viewMode = 'map'; 
+  viewMode = 'list'; 
   platform: any; 
   svg = `<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g filter="url(#filter0_d_2281_9719)">
@@ -86,106 +58,45 @@ export class SubmissionsComponent implements AfterViewInit {
     icon = new H.map.Icon(this.svg, {
       anchor: { x: 24, y: 58 } // x: center horizontally, y: slightly below the bottom tip of the marker
   });
-  forms = ['denisgordiyenya@gmail.com', 'john.doe@gmail.com', 'alice.smith@gmail.com'];
-  statuses = ['Uncomplete', 'Complete', 'Needs Review'];
-  
-  selectedForm: string | null = null;
-  searchTerm: string | null = null;
-  selectedStatus: string | null = null; 
-  
-  selectedDate: Date | null = null;
-  submissions = [
-    {
-      title: 'Work Flow: Requires Location',
-      from: 'denisgordiyenya@gmail.com',
-      to: 'denisgordiyenya@gmail.com',
-      dueDate:  '2024-12-09',
-      status: {key:'Uncomplete', value:'Uncomplete'},
-      coordinates: {  lat: 43.6707974, lng: -79.347015 }
-    },
-    {
-      title: 'Work Flow: Requires Location',
-      from: 'john.doe@gmail.com',
-      to: 'jane.doe@gmail.com',
-      dueDate:  '2024-12-08',
-      status: {key:'Complete', value:'Complete'},
-      coordinates: { lat: 43.653908, lng: -79.384293}
-    },
-    {
-      title: 'Work Flow: Requires Location',
-      from: 'alice.smith@gmail.com',
-      to: 'bob.smith@gmail.com',
-      dueDate:  '2024-12-07',
-      status: {key:'Uncomplete', value:'Uncomplete'},
-      coordinates: { lat:43.662301, lng: -79.329356 }
-    },
-    {
-      title: 'Work Flow: Requires Location',
-      from: 'alice.smith@gmail.com',
-      to: 'bob.smith@gmail.com',
-      dueDate: '2024-12-06',
-      status: {key:'needreview', value:'Needs Review'},
-      coordinates: {  lat: 43.662301, lng: -79.395293 }
+ 
+ 
+  getStatusClass(status: number): string {
+    const num = Number(status);
+    switch (num) {
+      case 1: return 'lowrisk';
+      case 2: return 'uncomplete';
+      case 3: return 'complete';
+      default: return 'unassigned';
     }
-  ];
-  
-  constructor(private cdr: ChangeDetectorRef) {}
+  }
+
+  getStatusLabel(status: number): string {
+    const num = Number(status);
+    switch (num) {
+      case 1: return 'Low Risk';
+      case 2: return 'Uncomplete';
+      case 3: return 'complete';
+      case 0: return 'Unassigned';
+      default: return '';
+    }
+  }
   ngAfterViewInit(): void {
     
     if (!this.map && this.mapContainer ) {
       this.renderMap(this.mapContainer .nativeElement.lastChild);
     }
   }
-  onFilterChange() {
-    if (this.viewMode === 'map') {
-      this.renderMap(this.mapContainer .nativeElement.lastChild);
-    }
-  }
-  onDateChange(event: MatDatepickerInputEvent<Date>): void {
-    this.selectedDate = event.value; 
-  }
-  get filteredSubmissions() {
-    return this.submissions.filter((submission) => {
-      const matchesForm = !this.selectedForm || submission.from === this.selectedForm;
-      const matchesStatus = !this.selectedStatus || submission.status.value === this.selectedStatus;
-      let matchesDate = true; 
-      if (this.selectedDate) {
-        const selectedDate = new Date(this.selectedDate).toDateString();
-        const submissionDate = new Date(submission.dueDate).toDateString();
-        matchesDate = selectedDate === submissionDate; // Compare the dates
-      }
-
-
-      const matchesSearchTerm = !this.searchTerm || 
-      submission.title.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-      submission.from.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-      submission.to.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-      submission.status.value.toLowerCase().includes(this.searchTerm.toLowerCase());
-
-      return matchesForm && matchesStatus && matchesDate && matchesSearchTerm;
-      
-    });
-  }
+  
   addMarkers(map : any): void {
    
     this.filteredSubmissions.forEach((markerData) => {
-      const marker = new H.map.Marker({ lat: markerData.coordinates.lat, lng: markerData.coordinates.lng });
+      const marker = new H.map.Marker({ lat: markerData.lat, lng: markerData.lng });
       marker.setIcon(this.icon);
       map.addObject(marker);
     });
   }
-  toggleView(event: MatButtonToggleChange) {
-    this.cdr.detectChanges();
-    console.log('Selected value:', event.value);
-    if ( event.value  == "map"){
-      this.renderMap(this.mapContainer .nativeElement.lastChild);
-    }
-    
-  }
-  exportData() {
-    console.log('Exporting data...');
-    alert('Data exported successfully!');
-  }
+  
+  
   renderMap(container: HTMLElement){
     setTimeout(() => {
       container.innerHTML = "";
@@ -217,3 +128,5 @@ export class SubmissionsComponent implements AfterViewInit {
   
   }
 }
+
+
